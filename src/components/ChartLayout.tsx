a072cc802cf66e52820f477990836917e049dca8
sohseyedi <sohseyedi@gmail.com>
2025-08-04 12:24:58 +0330

    change name component and add css classes

--- src/components/ChartLayout.tsx
+++ src/components/ChartLayout.tsx
@@ -1,17 +1,20 @@
 import { type ChartDefinition, type SingleSeriesData, type MultiSeriesData } from '../types/chart';
 import MultiLineChart from './MultiLineChart';
 import SingleLineChart from './SingleLineChart';
+import './styles/layout.css';
 
 type Props = {
   chart: ChartDefinition;
 };
 
-export default function ChartRenderer({ chart }: Props) {
+export default function ChartLayout({ chart }: Props) {
+  if (!chart.data?.length) return <p>No data available for this chart</p>;
+
   const isMultiSeries = Array.isArray(chart.data[0][1]);
 
   return (
-    <div>
-      <h2>{chart.title}</h2>
+    <div className="chart-container">
+      <h2 className="chart-title">{chart.title}</h2>
       {isMultiSeries ? (
         <MultiLineChart data={chart.data as MultiSeriesData[]} />
       ) : (
